48c77ef3fb3981111ac8cf07d2470be77f816429
Chukwu-Godgive <chukwugodgive@gmail.com>
2022-09-11 07:34:42 -0700

    4-puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/*
- * Description: How to use puts to print in C
- */
+/*main - How to use puts to print in C */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
