16bdf933a02d161a2abf2a474782e946357d66cf
Chukwu-Godgive <chukwugodgive@gmail.com>
2022-09-11 08:42:10 -0700

    101-quote.c updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Standard error.
+ * Return: 1 always
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
