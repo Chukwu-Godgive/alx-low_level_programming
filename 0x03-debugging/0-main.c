281584299af8f7bf25e91b4091d783dc21bc861e
Chukwu-Godgive <chukwugodgive@gmail.com>
2022-09-17 03:57:13 -0700

    0-main.c fixed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * main - function that prints if integer is positive or negative
+ * main - function that tests and prints if integer is positive or negative
  * Return: 0 always
  */
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
+
 	positive_or_negative(i);
+
 	return (0);
 }
